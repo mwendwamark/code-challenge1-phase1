37c05b2b2c27d6c24af88afbd0348dbacd67a662
mark mwendwa <mwendwamark310@gmail.com>
2023-03-18 17:28:59 +0300

    first project

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # code-challenge1-phase1
+# This program is based on grading the students marks.
+The marks are between 0 marks to 100 marks.
+Anything below 0 (eg. -74  ) , above 100 (eg. 301) or a string, counts as an invalid grade, and it returns a message "This is an invalid mark. Please try again"
+
+The are five grades and each grade has a limit according to the marks namely:
+*Grade "A" = anything > 79 && < 101
+*Grade "B" = anything > 59 && < 78
+*Grade "C" = anything > 48 && < 58
+*Grade "D" = anything > 39 && < 48
+*Grade "E" = anything > 0  && <= 40
+
+
+# code-challenge1-phase1
+
--- script.js
+++ script.js
@@ -1,3 +1,44 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+//CODE CHALLENGE 1
+//A > 79, B - 60 to 79, C -  59 to 49, D - 40 to 49, E - less 40
+function getStudentGrade(grade){
+   
+    
+   
+    
+    //Validation of grades
+    if (grade=== undefined || isNaN(grade)|| grade<0 || grade>100){
+        return "This is an invalid mark.Please try again"
+    }
+    //These are the conditions of the grades
+if (grade >79 && grade<101) {
+    console.log(gradeSelector)
+    return "A"
+    
+}
+else if (grade> 59 && grade< 78) {
+    console.log(gradeSelector)
+    return "B"
+    
+}
+else if (grade>48 && grade< 58) {
+    console.log(gradeSelector)
+
+    return "C"
+    
+}
+else if (grade>39 && grade<=48) {
+    console.log(gradeSelector)
+    return "D"
+    
+}
+else if(grade>0 && grade<=40) {
+    console.log(gradeSelector)
+    return "E"
+}
+ 
+else{ 
+    return "This is an invalid mark.Please try again"}
+}
+
+
+
