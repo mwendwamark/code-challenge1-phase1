9d56fb712b8315d2b20f55d9924cb2b0c1435e9b
mark mwendwa <mwendwamark310@gmail.com>
2023-03-18 23:20:47 +0300

    add script.js, connect to index.html

--- script.js
+++ script.js
@@ -42,3 +42,47 @@ else{
 getStudentGrade()
 
 
+
+
+
+
+
+/** Speed Detector
+ * Input is speed of car
+ * Speed limit is 70
+ * When speed of car is less than 70 print "Ok"
+ * For every 5 km/s above the speed limit give the driver one demerit point and print the total number of demerit points.
+ * When driver gets more than 12 demerit points print "License suspended"
+*/
+function detectOverspeeding(speedOfCar) {
+    // Validating
+    // Validation
+    if (speedOfCar === undefined || isNaN(speedOfCar)
+    ){
+        return "Invalid value"
+    }
+    // conditions
+    if (speedOfCar<=70){ 
+        return "Ok"
+    }
+    else {
+        excessSpeedLimit = speedOfCar - 70;
+        points = 0;
+        while (excessSpeedLimit > 0){
+            excessSpeedLimit = excessSpeedLimit - 5;
+            points = points + 1;
+            if(points > 12) {
+                return "License suspended";
+            }
+        excessSpeedLimit = (speedOfCar - 70)/5;
+        points = Math.floor(excessSpeedLimit);
+        if (points>12){
+            return "License suspended"
+        }
+        else{
+            return ("Points:" + points)
+        }
+    
+    }
+
+    }}
